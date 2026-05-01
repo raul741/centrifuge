f4a888ccfe2b0a366762517a0224a17257e4ba7b
raul <raul@bulgariu.xyz>
2026-05-01 19:43:07 +0200

    add source files

---
+++ README.md
@@ -0,0 +1,14 @@
+# centrifuge
+
+Tool created for converting Factorio spritesheets into animated GIFs.
+
+## Usage
+
+```bash
+git clone https://github.com/raul741/centrifuge
+cd centrifuge
+python3 -m venv .venv
+source ./.venv/bin/activate
+pip install -r requirements.txt
+./centrifuge.py -i assembling-machine-3.png -o assembling-machine-3.gif
+```
---
+++ centrifuge.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import argparse, os
+from PIL import Image
+
+def main():
+    """
+    Extract images from Factorio spritesheets and convert them into a GIF file
+    """
+    PATH = "./temp/"
+    if not os.path.isdir(PATH):
+        os.mkdir(PATH)
+
+    p = argparse.ArgumentParser()
+    p.add_argument("-i", "--input", required=True)
+    p.add_argument("-o", "--output", required=True)
+    args = p.parse_args()
+    spritesheet = Image.open(args.input)
+    width, height = spritesheet.size
+
+    horizontal_items = int(input("Number of horizontal items: "))
+    vertical_items = int(input("Number of vertical items: "))
+
+    real_width = width / horizontal_items
+    real_height = height / vertical_items
+
+    left = 0
+    up = 0
+
+    right = real_width
+    down = real_height
+    files = []
+
+    for i in range(vertical_items):
+        for l in range(horizontal_items):
+            print(f"{left} {up} | {right*(l+1)} x {down*(i+1)}")
+            frame = spritesheet.crop((left, up, right*(l+1), down*(i+1)))
+            save = f"{PATH}/frame_{i}-{l}.png"
+            frame.save(save)
+            files.append(save)
+            left += right
+        left = 0
+        up += down
+    frames = [Image.open(image) for image in files]
+    frames[0].save(args.output, save_all=True, append_images=frames[1:], duration=0, loop=0, lossless=True, optimize=False)
+
+
+if __name__ == "__main__":
+    main()
